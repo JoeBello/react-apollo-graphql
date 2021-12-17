231b15d048610d7b5f40ed33f9772c5947722584
Joe Bello <JoeBello82@gmail.com>
2021-12-16 19:48:40 -0500

    feat: exchange rates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ExchangeRates from './ExchangeRates';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React-GraphQL</h1>
       </header>
+	  <main>
+		  <ExchangeRates />
+	  </main>
     </div>
   );
 }
